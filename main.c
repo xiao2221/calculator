50959abd6aa7dd7b53adc00064e0c6c46e4c3dac
xiao2221 <xiaoliang@1000phone.com>
2015-12-15 16:08:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
-//做一个计算器，满足+-*/，以及十进制转换成二进制、八进制、十六进制。
+/*
+项目要求：做一个计算器，满足加、减、乘、除，以及十进制转换成二进制、八进制、十六进制。
+时间：2015.12.15
+作者：xiaoliang
+*/
 #include <stdio.h>
 #include "menu.h"
 int main(void)
 {
 	int num;
 	char sel,dis;
-	
 BE:
-	printf("请选择使用方法（1、计算；2、转换）:\n");
-	scanf("%d%c",&num,&dis);
+	system("clear");	
+	printf("========================\n");
+	printf("欢迎使用xiaoliang计算器！\n");
+	printf("1、计算\n");
+    	printf("2、转换\n");
+	printf("========================\n");
+	printf("\n请选择计算类型:");
+
+    	scanf("%d%c",&num,&dis);
 	switch(num){
 		case 1:
 			calculate();
@@ -17,11 +27,11 @@ BE:
 			convert();
 			break;
 		default:
-			printf("输入有误\n");
+			printf("\n(⊙o⊙)哦，输入有误！\n");
 			break;
 	}
 
-	printf("是否继续(Y/N)?\n");
+	printf("\n是否继续(Y/N)?\n");
 	scanf("%c%c",&sel,&dis);
 	if(sel=='Y'||sel=='y')
 		goto BE;
