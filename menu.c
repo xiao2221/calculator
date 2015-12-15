32ed7e8d5fb857c2e9840b9a2ff471933814ff66
xiao2221 <xiaoliang@1000phone.com>
2015-12-15 16:05:45 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,29 +1,83 @@
 #include <stdio.h>
 
+/*
+ 私有函数add()
+ 函数功能：加法运算
+ 函数参数：
+ int a:被加数
+ int b:加数
+ 返回值：运算（a+b）之后的值
+*/
 int add(int a,int b)
 {
 	return a+b;
 }
+/*
+ 私有函数sub()
+ 函数功能：减法运算
+ 函数参数：
+ int a:被减数
+ int b:减数
+ 返回值：运算（a-b）之后的值
+*/
 int sub(int a,int b)
 {
 	return a-b;
 }
+/*
+ 私有函数mul()
+ 函数功能：乘法运算
+ 函数参数：
+ int a:被乘数
+ int b:乘数
+ 返回值：运算（a*b）之后的值
+*/
 int mul(int a,int b)
 {
 	return a*b;
 }
+/*
+ 私有函数div()
+ 函数功能：除法运算
+ 函数参数：
+ int a:被除数
+ int b:除数
+ 返回值：运算（a/b）之后的值
+*/
 int div(int a,int b)
 {
 	return a/b;
 }
+
+/*
+ 私有函数eight()
+ 函数功能：将十进制数值转换成八进制，并输出
+ 函数参数：
+ int a:被转换的十进制数
+ 返回值：无
+*/
 void eight(int a)
 {
-	printf("%o\n",a);
+	printf("%d对应的八进制数是：%o\n",a,a);
 }
+/*
+ 私有函数sixteen()
+ 函数功能：将十进制数值转换成十六进制，并输出
+ 函数参数：
+ int a:被转换的十进制数
+ 返回值：无
+*/
 void sixteen(int a)
 {
-	printf("%x\n",a);
+	printf("%d对应的十六进制数是：%x\n",a,a);
 }
+/*
+ 私有参数two()
+ 函数功能：将十进制数值转换成二进制，并输出
+ 函数参数：
+ int a:被转换的十进制数
+ 返回值：无
+*/
 void two(int a)
 {
 	int b[64],i=0;
@@ -32,18 +86,29 @@ void two(int a)
 		a/=2;
 	}
 	b[i]=1;
+	printf("%d对应的二进制数是：",a);
 	for(int j=i;j>=0;j--)
 		printf("%d",b[j]);
 	printf("\n");
 }
 
+/*
+ 导出的公共函数
+ 函数功能：四则运算函数，可满足加、减、乘、除四种运算
+ 函数参数：无
+ 返回值：无
+*/
 void calculate(void)
 {
 	char fu,dis;
 	int num1,num2,ret=0;
-	printf("请选择运算法则(+-*/):\n");
+	printf("+:加法运算\n");
+	printf("-:减法运算\n");
+	printf("*:乘法运算\n");
+	printf("/:除法运算\n");
+	printf("请选择运算法则:");
 	scanf("%c%c",&fu,&dis);
-	printf("请输入两个数:\n");
+	printf("请输入两个数:");
 	scanf("%d%d%c",&num1,&num2,&dis);
 
 	switch(fu){
@@ -60,19 +125,32 @@ void calculate(void)
 			ret=div(num1,num2);
 			break;
 		default:
-			printf("您的输入有误\n");
+			printf("(⊙o⊙)哦，您的输入有误\n");
 	}
+    
+	printf("\n****************\n");
 	printf("%d %c %d = %d\n",num1,fu,num2,ret);
+	printf("****************\n");
 }
-
+/*
+ 导出的公共函数
+ 函数功能：将已知十进制数值转换成对应的八进制、十六进制、二进制数值
+ 函数参数：无
+ 返回值：无
+*/
 void convert(void)
 {
 	char fu,dis;
 	int num;
-	printf("请输入（O八进制、H十六进制、B二进制）\n");
+	printf("O八进制\n");
+	printf("H十六进制\n");
+	printf("B二进制\n");
+	printf("请选择：");
 	scanf("%c%c",&fu,&dis);
-	printf("请输入要转换的数值:\n");
+	printf("请输入要转换的数值:");
 	scanf("%d%c",&num,&dis);
+    
+	printf("\n*************************\n");
 	switch(fu){
 		case 'O':
 		case 'o':
@@ -87,6 +165,7 @@ void convert(void)
 			two(num);
 			break;
 		default:
-			printf("您的输入有误\n");
+			printf("(⊙o⊙)哦，您的输入有误\n");
 	}
+	printf("*************************\n");
 }
